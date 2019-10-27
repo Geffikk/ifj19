b4b626995c226df400fe2fd7e686679045037926
Maroš Geffert <xgeffe00@vutbr.cz>
2019-10-27 17:47:27 +0100

    #1 Dynamic string

---
+++ lexem_string.c
@@ -0,0 +1,89 @@
+/****** LEXEM_STRING ******
+ * @author : Maros Geffert <xgeffe00@stud.fit.vutbr.cz>
+ * Subject : IFJ
+ * Project : Compiler implementation imperativ language IFJ
+ * @brief : Lexem_string
+***************************/
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "lexem_string.h"
+
+void lexem_string_clear(Lexem_string *s)
+{
+    s->length_of_lexem_string = 0;
+    s->string[s->length_of_lexem_string] = '\0';
+}
+bool lexem_string_init(Lexem_string *s)
+{
+    s->string = (char*)malloc(sizeof(char));
+    if (!s->string){
+        return false;
+    }
+
+    s->length_of_lexem_string = 0;
+    s->string[s->length_of_lexem_string] = '\0';
+    s->allocated_size_of_lexem_string = sizeof(char);
+
+    return true;
+}
+
+bool add_char_to_lexem_string(Lexem_string *s, char c)
+{
+    if(s->length_of_lexem_string + 1 >= s->allocated_size_of_lexem_string)
+    {
+        if(!(s->string = (char*)realloc(s->string, s->length_of_lexem_string + 8)))
+        {
+            return false;
+        }
+    }
+    s->allocated_size_of_lexem_string = s->length_of_lexem_string + 8;
+    s->string[s->length_of_lexem_string++] = c;
+    s->string[s->length_of_lexem_string] = '\0';
+    return true;
+}
+
+bool add_string_to_lexem_string(Lexem_string *s, const char *add_string)
+{
+    unsigned length_of_new_string = (unsigned int) strlen(add_string);
+
+    if (s->length_of_lexem_string + length_of_new_string + 1 >= s->allocated_size_of_lexem_string){
+        char new_size = length_of_new_string + s->length_of_lexem_string + 1;
+        s->string = (char*)realloc(s->string, new_size);
+        if (!s->string){
+            return false;
+        }
+        s->allocated_size_of_lexem_string = new_size;
+    }
+
+    s->length_of_lexem_string = s->length_of_lexem_string + length_of_new_string + 1;
+    strcat(s->string, add_string);
+    s->string[s->length_of_lexem_string] = '\0';
+}
+
+bool cmp_lexem_string_with_another_string(Lexem_string *s, const char *cmp_string)
+{
+    int result = strcmp(s->string, cmp_string);
+
+    if (result == 0){
+        return true;
+    }
+
+    return false;
+}
+
+bool copy_lexem_string_to_attribute_string(Lexem_string *s, Lexem_string *d)
+{
+
+    if (s->length_of_lexem_string + 1 >= d->allocated_size_of_lexem_string){
+        d->string = (char *) realloc(d->string, s->length_of_lexem_string+1);
+        if(!d->string){
+            return false;
+        }
+    }
+
+    strcpy(d->string, s->string);
+    d->length_of_lexem_string = s->length_of_lexem_string;
+    return true;
+}
