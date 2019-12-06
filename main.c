b2d8b6701959dc2d3846321b19418cc00daf83bc
Maros Geffert <xgeffe00@vutbr.cz>
2019-12-06 10:23:21 +0100

    #1 second final

--- main.c
+++ main.c
@@ -11,11 +11,13 @@
 int main()
 {
     FILE* source_file;
-    source_file = fopen("/home/patres/CLionProjects/IFJ_2019/test2.txt", "r");
+    /* source_file = fopen("/home/patres/CLionProjects/IFJ_2019/test1.txt", "r");
     if (source_file == NULL){
         return -1;
-    }
+    } 
+    */
 
+    source_file = stdin;
     int result;
 
     set_source_file(source_file);
